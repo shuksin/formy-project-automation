cfe55a1a39dbde11f9d9d502d467fb2b94132cd8
May Lee <may.shuk.sin.lee@oracle.com>
2019-12-02 19:41:40 +0800

    Initial commt

---
+++ 04_04/start/test/Dropdown.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Dropdown {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Ruby26-x64\\Exercise Files\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/dropdown");
+
+        WebElement dropDownMenu = driver.findElement(By.id("dropdownMenuButton"));
+        dropDownMenu.click();
+
+        WebElement autocompleteItem = driver.findElement(By.id("autocomplete"));
+        autocompleteItem.click();
+
+
+        driver.quit();
+    }
+}
---
+++ Ch03/03_03/start/users_page.rb
@@ -0,0 +1,21 @@
+class UsersPage
+
+# css selectors
+SUCCESS_BANNER = {id: 'flash_success'}
+
+attr_reader :driver
+
+# class methods
+def initialize (driver)
+	@driver = driver
+end
+
+
+
+def get_banner_text()
+  banner = @driver.find_element(SUCCESS_BANNER)
+  banner_text = banner.text
+end
+
+
+end
